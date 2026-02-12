b3c7775ef882e3604592de99d96468ab81211d87
Silas Rodrigues Nascimento <110751872+SilasNR@users.noreply.github.com>
2026-02-12 19:18:57 +0000

    Mudanças no IP main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import 'reflect-metadata';
 
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.listen(process.env.PORT ?? 3001);
+
+  app.enableCors({
+    origin: '*', // Em produção, substitua pelo link do seu frontend
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
+
 bootstrap();
