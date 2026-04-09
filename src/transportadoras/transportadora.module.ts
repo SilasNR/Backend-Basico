dcbc4ba42e918aa466713431eca2a545af20181d
Silas Rodrigues <silasnas2018@gmail.com>
2026-04-09 17:11:37 -0300

    Correção de Erro: Imports no Modulo Transportadora

--- src/transportadoras/transportadora.module.ts
+++ src/transportadoras/transportadora.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TransportadoraService } from './transportadora.service';
 import { TransportadoraController } from './transportadora.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Transportadora } from './entities/transportadora.entity';
+import { Estado } from './entities/estado.entity';
+import { Regiao } from './entities/regiao.entity';
 
 @Module({
+  imports: [
+      TypeOrmModule.forFeature([Transportadora, Estado,Regiao]), // <-- Importa o repositório aqui!
+    ],
   controllers: [TransportadoraController],
   providers: [TransportadoraService],
 })
