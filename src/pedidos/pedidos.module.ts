c0e98c84eb46f90f1b807fea06508a6d1a1182ad
Silas Rodrigues <silasnas2018@gmail.com>
2026-04-14 17:28:51 -0300

    Ajuste (Modulo) Pedido

--- src/pedidos/pedidos.module.ts
+++ src/pedidos/pedidos.module.ts
@@ -5,10 +5,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Pedido } from './entities/pedido.entity';
 import { PedidoLista } from './entities/pedido-lista.entity';
 import { Produto } from '../produto/entities/produto.entity'; 
+import { Cotacao } from './entities/cotacao.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Pedido, PedidoLista,Produto,]), // <-- Importa o repositório aqui!
+    TypeOrmModule.forFeature([Pedido, PedidoLista,Produto,Cotacao]), // <-- Importa o repositório aqui!
   ],
   controllers: [PedidosController],
   providers: [PedidosService],
